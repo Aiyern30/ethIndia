020c3862c890f81aa1098519af8932dc7cd59f59
Aiyern30 <ianbian2@gmail.com>
2025-11-23 15:59:28 +0800

    `Updated dependencies in package-lock.json and package.json`

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,17 +1,31 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
-const dotenv = require("dotenv");
-dotenv.config();
+require("dotenv").config();
 
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require("@nomicfoundation/hardhat-verify");
 
-const config = {
+module.exports = {
   solidity: "0.8.20",
   networks: {
     sepolia: {
       url: process.env.RPC_URL,
-      accounts: [process.env.PRIVATE_KEY]
-    }
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+    customChains: [
+      {
+        network: "sepolia",
+        chainId: 11155111,
+        urls: {
+          apiURL: "https://api-sepolia.etherscan.io/api",
+          browserURL: "https://sepolia.etherscan.io"
+        }
+      }
+    ]
+  },
+  sourcify: {
+    enabled: false
   }
 };
-
-module.exports = config;
